1c266f358c619aec196d846a90f40565649e8de3
YOUR_USERNAME <shaikhamaannn100@gmail.com>
2024-09-22 21:58:28 +0530

    Set node env and connected to mongoose db

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+// Handle Rejected Promises Globally
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! 💣 Shutting down...");
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
 dotenv.config({ path: "./config.env" });
 
+const DB = process.env.DATABASE.replace(
+  "<db_password>",
+  process.env.DATABASE_PASSWORD
+);
+
+// Connecting to Mongoose
+async function main() {
+  const connection = await mongoose.connect(DB);
+  console.log("DB Connection Successful");
+}
+
+main();
+
+// Runs the Server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, (req, res) => {
-  console.log(`LISTENING ON PORT ${port}`);
+  console.log(`LISTENING ON PORT ${PORT}`);
+});
+
+// Handle Rejected Promises Globally
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! 💣 Shutting down...");
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
